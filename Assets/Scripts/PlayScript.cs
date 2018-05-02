99c5a02bc02bbb1767d2272d9c62c37ac12c5ade
Almazikman <korobko416@gmail.com>
2018-05-02 22:08:29 +0300

    Изменения

---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScript : MonoBehaviour {
+
+	public GameObject animBackground;
+
+	void Start () {
+		animBackground.SetActive (true);
+	}
+
+	void Update () {
+		
+	}
+}
