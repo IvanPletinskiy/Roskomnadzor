81094e3bb32c2cbbe418ba9a4a6d67d8c5854be6
Almazikman <korobko416@gmail.com>
2018-05-02 22:00:53 +0300

    Вот

---
+++ Assets/Scripts/MainMenuScriot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScriot : MonoBehaviour {
+
+	public Camera mainCamera;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Ray ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				switch (hit.collider.name) {
+				case "Play":
+					SceneManager.LoadScene ("Play");
+					break;
+				}
+			}
+		}
+	}
+}
